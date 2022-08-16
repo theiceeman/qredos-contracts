d9861baf483cebda0ca3be00fe2022f8aa49102c
Ebube Okorie <okorieebube1@gmail.com>
2022-08-16 15:57:32 +0100

    feat: escrow contract completed

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,24 +1,54 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.0;
 
-import "./interfaces/IPool.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-contract Escrow {
-  // Pool the Escrow is associated with
-  IPool public pool;
+contract Escrow is IERC721Receiver {
 
-  constructor(IPool _pool){
-    pool = _pool;
-  }
+  enum EscrowStatus {NFT_DEPOSITED, NFT_WITHDRAWN}
+
+    address public owner;
+    address public immutable borrowerAddress;
+    uint256 public immutable tokenId;
+    address public immutable tokenAddress;
+    EscrowStatus public status;
+
+    constructor(
+        address _borrowerAddress,
+        uint256 _tokenId,
+        address _tokenAddress
+    ) {
+        owner = msg.sender;
+        borrowerAddress = _borrowerAddress;
+        tokenId = _tokenId;
+        tokenAddress = _tokenAddress;
+    }
 
 
-  // store an NFT
-  function deposit() external{
+    function deposit(uint256 tokenId, address tokenAddress) external onlyOwner returns(bool){
+       require(tokenAddress !== 0, "Escrow: address is zero address!");
+       require(
+        ERC721(tokenAddress).safeTransferFrom(msg.sender, address(this), tokenId),
+        "Escrow: Transfer failed!")
+       status = EscrowStatus.NFT_DEPOSITED;
+       return true;
 
-  }
+    }
 
-  // remove an NFT from the contract
-  function withdraw() external{
+    function withdraw(address newNftOwnerAddress) external onlyOwner {
+       require(newNftOwnerAddress !== 0, "Escrow: address is zero address!");
+       require(
+        ERC721(tokenAddress).safeTransferFrom(address(this), newNftOwnerAddress, tokenId),
+        "Escrow: Transfer failed!")
+       status = EscrowStatus.NFT_WITHDRAWN;
+       return true;
 
-  }
+    }
+
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Ownable: caller is not owner");
+        _;
+    }
 }
