50ce508cb558eebe10d84c1fa95a42cfccbfff73
Daniel Onabiyi <45369564+danizzy@users.noreply.github.com>
2022-08-18 12:43:51 +0100

    Create borrowerbuy.sol

---
+++ contracts/borrowerbuy.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+
+contract borrowerbuy {
+
+   constructor(
+        address _borrowerAddress,
+        uint256 _tokenId,
+        address _tokenAddress
+    ) {
+        owner = msg.sender;
+        borrowerAddress = _borrowerAddress;
+        tokenId = _tokenId;
+        tokenAddress = _tokenAddress;
+    }
+
+ 
+  
+
+
+  
+
+
+   function borrowerbuy(address borroweraddress, uint256 tokenId) public payable {
+   
+   
+    
+
+  }
