4e83c1892b646ab5b2dd3f0aa2267d7c9f00d0c6
Ebube Okorie <okorieebube1@gmail.com>
2022-08-21 06:20:12 +0100

    feat/qredos-oracle

---
+++ contracts/models/schema.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.1;
+
+// struct loanRequest{
+//     address _lendingToken;
+//     uint256 _marketplaceId;
+//     uint256 _principal;
+//     uint32 _duration;
+//     uint16 _APY;
+//     uint32 _paymentCycle;
+//     bytes32 _metadataURI;
+//     address _receiver;
+// }
